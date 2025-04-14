53e17f4c2681af5ffa517954df3452381fea2039
NilaDoze <niladoze@gmail.com>
2025-04-14 16:30:56 +0200

    Changed:HM bg-color, logo-size, bio-text, separator + B card-padding + AW hero-title; Added:AW card

---
+++ scripts.js
@@ -0,0 +1,33 @@
+function copyToClipboard() {
+    const linkInput = document.getElementById("shareLink");
+    if (!linkInput) return;
+
+    navigator.clipboard.writeText(linkInput.value)
+        .then(() => alert("Link copied! Now go and tell everyone you found this link while fighting off a dragon."))
+        .catch(err => console.error("Failed to copy: ", err));
+}
+
+// Get the image and audio elements
+const audioImage = document.querySelector('.audio_img');
+const audioPlayer = document.getElementById('audioPlayer');
+
+// Toggle play/pause on image click
+audioImage.addEventListener('click', () => {
+    if (audioPlayer.paused) {
+        audioPlayer.play();
+    } else {
+        audioPlayer.pause();
+    }
+});
+
+const toggleImage = document.getElementById('toggleImage');
+
+// Define the two image sources
+const image1 = 'Images/noaudio.png';
+const image2 = 'Images/audio.png';
+
+// Add click event listener
+toggleImage.addEventListener('click', () => {
+// Check current image source and switch
+toggleImage.src = toggleImage.src.includes(image1) ? image2 : image1;
+});
